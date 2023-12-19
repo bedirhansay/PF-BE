980ca0edaca721eedcb6094fbd91656fe44fb138
bedirhansay <bedirhan.sayy@gmail.com>
2023-12-19 23:18:04 +0300

    s

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { mainRouter } from "./src/routes";
-import { CustomErrorHandler } from "./src/middleware";
 import { connectDB } from "./src/config/connectDB";
 
 dotenv.config();
@@ -13,13 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api", mainRouter);
+app.use("/api", (req, res) => {
+  res.send("api");
+});
 
 app.get("/", (req, res) => {
   res.send("Bedirhansayapi");
 });
-
-app.use(CustomErrorHandler);
 
 connectDB()
   .then(() => {
--- src/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./usersModel";
-export * from "./SkillsModels";
