b2c40297f3bec21536ac88e8c29da23aa530009f
bedirhansay <bedirhan.sayy@gmail.com>
2023-12-19 20:21:03 +0300

    vercel node

--- index.ts
+++ index.ts
@@ -20,9 +20,15 @@ app.use("/calculator", calculatorRouter);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`[-- * Server is running on port * --] ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`[-- * Server is running on port * --] ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("----[Database connection error----]:", err);
+  });
 
 export default app;
