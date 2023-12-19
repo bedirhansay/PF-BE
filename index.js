c52cbc7225b246d4a90dbd58e4676fffc8668cee
bedirhansay <bedirhan.sayy@gmail.com>
2023-12-19 12:21:19 +0300

    fins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { connectDB } = require("./src/config/connectDB");
 
 dotenv.config();
 const app = express();
@@ -17,9 +18,15 @@ app.get("/", (req, res) => {
   res.send("Bedirhansayapi");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`[-- * Server is running on port * --] ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`[-- * Server is running on port * --] ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("----[Database connection error----]:", err);
+  });
 
 module.exports = app;
