9a001495e208db8f592841e2525a303c3788f9c4
bedirhansay <bedirhan.sayy@gmail.com>
2023-12-19 11:53:22 +0300

    son

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-const PORT = 4000;
 
 const app = express();
+const PORT = 4000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-// Routes
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
@@ -19,8 +15,5 @@ app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
 });
 
-app.listen(PORT, () => {
-  console.log(`[-- * Server is running on port * --] ${PORT}`);
-});
-
+// Export the Express API
 module.exports = app;
