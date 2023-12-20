f2218e94629ff60d15e579768bee136ac4b83823
bedirhansay <bedirhan.sayy@gmail.com>
2023-12-20 08:06:54 +0300

    token structure

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -7,16 +7,15 @@ dotenv.config();
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   const { JWT_SECRET } = process.env;
 
+  console.log(JWT_SECRET);
+
   if (!JWT_SECRET) {
     return res.status(500).json({
       message: "JWT_SECRET is not defined in the environment variables.",
     });
   }
 
-  const token = req.header("Authorization")
-    ? //@ts-ignore
-      req.header("Authorization").split("Bearer ")[1]
-    : null;
+  const token = req.header("Authorization")?.split("Bearer: ")[1] || null;
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
